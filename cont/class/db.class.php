5e2948508257f63515df2f2213927d0a95cff1f9
Eric Laufer <drericlaufer@gmx.de>
2023-10-22 14:04:48 +0200

    adding db config

--- cont/class/db.class.php
+++ cont/class/db.class.php
@@ -50,7 +50,7 @@ class db
         mb_regex_encoding( 'UTF-8' );
         mysqli_report( MYSQLI_REPORT_STRICT );
         try {
-            $this->link = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_BASE );
+            $this->link = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_NAME );
             $this->link->set_charset( "utf8" );
         } catch ( Exception $e ) {
             die( 'Unable to connect to database' );
